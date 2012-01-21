066724fc6af699a18223946482b84390773643fc
unknown <Dell@Index.(none)>
2012-01-21 23:07:09 +0600

    first commit

---
+++ fmv.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import id3reader
+
+src_folder = "e:\\misc\\test\\"
+
+albums = list()
+
+for file in os.listdir(src_folder):
+    if ".mp3" in file:
+        id3r = id3reader.Reader(src_folder+file)
+        album_name = id3r.getValue('album')
+        #somehow id3r.getValue('year') didn't work for me
+        year = id3r.getValue('TDRC')
+        new_album = True
+        for title in albums:
+            if album_name == title:
+                new_album = False
+        if new_album:
+            os.mkdir(src_folder+'('+year+')'+' '+album_name)
+            albums.append(album_name)
+        shutil.move(src_folder+file,src_folder+'\\''('+year+')'+' '+album_name+'\\'+file)
+    
+    
